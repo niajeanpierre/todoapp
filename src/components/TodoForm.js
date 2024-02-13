11e1ad8fea10c178d99380852191be26bfcca3e3
niajeanpierre <nia.jeanpierre@mykenzie.snhu.edu>
2024-02-13 12:02:06 -0500

    added functionality to edit, delete and add tasks

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,20 +1,33 @@
 import React, {useState} from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = (/* `{addTodo}` is a destructured prop passed to the `TodoForm` component. It
+allows the `TodoForm` component to access the `addTodo` function from its
+parent component. This function is typically used to add a new todo item to
+a list or perform any other action related to adding a todo. */
+{addTodo}) => {
   /* `const [value, setValue] = useState("")` is using the `useState` hook in React to create a state
   variable called `value` and a function called `setValue` to update the value. The initial value of
   `value` is set to an empty string `""`. */
   const [value, setValue] = useState("")
+
+  /**
+   * The handleSubmit function prevents the default form submission behavior and adds a todo item.
+   */
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    addTodo(value);
+
+    setValue("");
+  }
   return (
-    <form className='TodoForm'>
-      <input type='text' className='todo-input' placeholder='Task' /* `onChange={(e) =>
-      console.log(e.target.value)}/>`
-      is an event handler that is
-      triggered when the value of the
-      input field changes. It logs the
-      current value of the input field
-      to the console. */
-      onChange={(e) => console.log(e.target.value)}/>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type='text' className='todo-input'value={value} placeholder='Create a Task'
+      /* `onChange={(e) => setValue(e.target.value)}/>` is an event handler that is triggered when the
+      value of the input field changes. It takes the event object `e` as a parameter and updates the
+      state variable `value` with the new value of the input field. `e.target.value` refers to the
+      value of the input field that triggered the event. */
+      onChange={(e) => setValue(e.target.value)}/>
       <button type="submit" className='todo-button'> + Add Task</button>
     </form>
   )
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TodoForm } from './TodoForm'
+/* The line `import { v4 as uuidv4 } from 'uuid';` is importing the `v4` function from the `uuid`
+library. The `uuid` library is used to generate unique identifiers. `v4` = version 4 UUIDs. */
+import { v4 as uuidv4 } from 'uuid'
+import { Todo } from './Todo'
+import { EditTodoForm } from './EditTodoForm'
+uuidv4()
 
 export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([])
+
+  /**
+   * The addTodo function adds a new todo item to the todos array.
+   */
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ])
+    console.log(todos)
+  }
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    )
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? {...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  }
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? {...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
   return (
     <div className='TodoWrapper'>
-      <TodoForm />
+      <h1>Let's Be Productive !</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            key={index}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
     </div>
   )
 }
